d64f220039ee67bd435d36b4cfc769759dd57e4c
boutoutben <boutout.ben@gmail.com>
2024-02-27 14:14:27 +0100

    first commit

---
+++ Readmi.md
@@ -0,0 +1,5 @@
+#love Christ 
+Ce site est un site pour mon entrainement 
+
+#installation 
+
---
+++ routes/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "../html/index.php"),];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Love christ";
